63ba7efd5d79769d47d75ce5a4a572cbb4aeb06a
angelazehlawe20 <angelazehlawe20@gmail.com>
2023-12-27 01:38:09 +0300

    create Priority in PrioritySeeder

--- database/seeders/PrioritySeeder.php
+++ database/seeders/PrioritySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Priority;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,25 @@ class PrioritySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Priority::create([
+            'name' => 'High Priority',
+            'description' => 'This is a high priority task',
+            'order' => Priority::HIGH,
+            'color_or_mark' => Priority::COLORS[Priority::HIGH]
+        ]);
+
+        Priority::create([
+            'name' => 'Medium Priority',
+            'description' => 'This is a medium priority task',
+            'order' => Priority::MEDIUM,
+            'color_or_mark' => Priority::COLORS[Priority::MEDIUM]
+        ]);
+
+        Priority::create([
+            'name' => 'Low Priority',
+            'description' => 'This is a low priority task',
+            'order' => Priority::LOW,
+            'color_or_mark' => Priority::COLORS[Priority::LOW]
+        ]);
     }
 }
