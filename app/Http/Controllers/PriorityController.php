cd18298cf5b7bc3a69b216cb42daf6da3b3370f0
angelazehlawe20 <angelazehlawe20@gmail.com>
2023-12-27 00:04:55 +0300

    get all priorities function

--- app/Http/Controllers/PriorityController.php
+++ app/Http/Controllers/PriorityController.php
@@ -2,13 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Traits\GeneralTrait;
 use App\Models\Priority;
 use Illuminate\Http\Request;
 
 class PriorityController extends Controller
-{
-    public function create(){
 
+{
+    use GeneralTrait;
+
+    public function getAll(Request $request,Priority $priority){
+        $sortBy=$request->input('sort_by','order');
+        $priorities=Priority::with(['order']);
+        switch($sortBy){
+            case 'priority':
+                $priorities->orderBy('order');
+                break;
+            case 'color':
+                $priorities->orderBy('color_or_mark');
+                break;
+            default:
+            return $this->ResponseTasksErrors('Invalid sorting parameter', 400);
+            break;
+        }
+        $sortedPri=$priorities->get();
+        return $this->ResponseTasks($sortedPri,'All priorities by '.$sortBy.':',200);
+
+    }
+
+
+    public function create(){
 
         $priority = Priority::create([
             'name' => 'High Priority',
