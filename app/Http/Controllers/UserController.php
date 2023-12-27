d8093b8d3c95492a20afe730282dee507edc60ba
angelazehlawe20 <angelazehlawe20@gmail.com>
2023-12-27 19:24:59 +0300

    get all users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Traits\GeneralTrait;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    use GeneralTrait;
+
+    public function getAll()
+{
+    $users = User::all();
+    return $this->ResponseTasks($users,'All users',200);
+}
+
+
 }
