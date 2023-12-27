6595ef1f6bde6ac8ba1726da18f97aee7a20b9a5
angelazehlawe20 <angelazehlawe20@gmail.com>
2023-12-27 19:34:01 +0300

    get one user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,5 +16,14 @@ class UserController extends Controller
     return $this->ResponseTasks($users,'All users',200);
 }
 
-
+public function getOneUser(Request $request,User $user)
+{
+    $u_id=$request->input('id');
+    $oneUser=User::where('id',$u_id)->get();
+    if(!$oneUser)
+    {
+        return $this->ResponseTasksErrors('User not found',404);
+    }
+    return $this->ResponseTasks($oneUser,'User id '.$u_id,200);
+}
 }
