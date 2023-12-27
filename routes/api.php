62c3f09d77b324680a2aca5a14ef38346c31636b
angelazehlawe20 <angelazehlawe20@gmail.com>
2023-12-27 19:38:05 +0300

    get all users and get one user Route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PriorityController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use App\Models\Priority;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -45,3 +46,7 @@ Route::post('/getColorForOrder',[PriorityController::class,'getColorForOrder']);
 Route::post('/createPriority',[PriorityController::class,'createPriority']);
 Route::post('/updatePriority',[PriorityController::class,'updatePriority']);
 Route::post('/deletPriority',[PriorityController::class,'deletPriority']);
+
+              ////user
+Route::post('/getAllUsers',[UserController::class,'getAll']);
+Route::post('/getOneUser',[UserController::class,'getOne']);
