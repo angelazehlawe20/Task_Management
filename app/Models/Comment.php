050b6874032ef161afe02ab3e861ebd6c7dd00f2
angelazehlawe20 <angelazehlawe20@gmail.com>
2024-01-11 16:55:50 +0300

    use softDeletes

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,15 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     protected $table='comments';
     protected $fillable=['user_id','task_id','content'];
     protected $casts=[
         'addition_date'=> 'datetime'
     ];
+    protected $dates=['deleted_at'];
 
 public function user()
     {
